4f5c8a135f2d9d0c87983e9684b0ba4feeecc7bb
louisreed73 <luis.sa1973@gmail.com>
2019-07-10 12:47:00 +0200

    cambios en index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,22 @@
 // console.log("Holassssss!!!!!!");
 
 
-function primera1() {
-    var b="B";
-    console.log(b)
-    return b
-}
+
 function primera0() {
     var a="A";
-    primera1();
-    console.log(a)
+    function primera1() {
+        var b = "B";
+        // console.log(a);
+        function primera2() {
+            var c="C";
+            console.log(b)
+        }
+        // return b
+        primera2();
+    };
+
+    primera1()
+    // console.log(a)
 }
 
 primera0();
