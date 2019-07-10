b73838a02bb23afe5f8ded7f104b4822bd3fc028
louisreed73 <luis.sa1973@gmail.com>
2019-07-10 12:27:48 +0200

    Inclusion de los archivos

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 
 
-// console.log("Hola!!!!!!")
+// console.log("Hola!!!!!!");
+
+
+function primera1() {
+    var b="B";
+    console.log(b)
+    return b
+}
+function primera0() {
+    var a="A";
+    primera1();
+    console.log(a)
+}
+
+primera0();
